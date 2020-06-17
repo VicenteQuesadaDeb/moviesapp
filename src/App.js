f17bb271f3d372d2c2185953792a88440b356301
Vicente Quesada Camón <vicentequesadadeb@gmail.com>
2020-06-17 12:34:39 +0200

    Title, name/genre inputs & submit button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <StyledTitle>MoviesApp</StyledTitle>
+      <StyledInputs>
+        <StyledNameInput
+          type="text"
+          name="movieNameInput"
+          value="Enter movie name"
+          onChange={() => {
+            console.log("MovieNameInput");
+          }}
+        ></StyledNameInput>
+        <StyledGenreInput
+          type="text"
+          name="movieGenreInput"
+          value="Enter movie genre"
+          onChange={() => {
+            console.log("MovieGenreInput");
+          }}
+        ></StyledGenreInput>
+      </StyledInputs>
+      <StyledSubmitButton onClick={() => console.log("Submit movie")}>Submit</StyledSubmitButton>
+    </>
   );
 }
 
 export default App;
+
+const StyledTitle = styled.h1`
+   {
+    padding-top: 35px;
+    padding-bottom: 20px;
+    text-align: center;
+    align-items: center;
+    background-color: #2c1545;
+    margin: auto;
+    height: 50px;
+    color: #f2f6ad;
+    font-weight: bold;
+  }
+`;
+
+const StyledSubmitButton = styled.button`
+   {
+    border-radius: 10px;
+    border: none;
+    text-decoration: none;
+    display: inline-block;
+    text-decoration: none;
+    display: inline-block;
+    border: none;
+    background-color: #0d0327;
+    color: #f4f2c9;
+    padding: 10px 50px;
+    padding-left: 40px;
+    padding-right: 40px;
+    margin-top: 10px;
+    margin: 10px 120px 10px 120px;
+
+  }
+`;
+
+const StyledInputs = styled.div`
+   {
+    display: grid;
+    grid-template-colums: 60% 40%;
+    grid-template-rows: auto auto;
+  }
+`;
+
+const StyledNameInput = styled.input`
+   {
+    grid-column: 1 / span 3;
+    background-color: #f4f2c9;
+  }
+`;
+
+const StyledGenreInput = styled.input`
+   {
+    grid-column: 1 / span 3;
+    background-color: #f4f2c9;
+  }
+`;
